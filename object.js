e196feacd79244bb549a698e2a27fe7d9334fa84
Andi Faizal <andifaizal647@gmail.com>
2023-05-13 00:24:13 +0700

    add object

--- object.js
+++ object.js
@@ -14,6 +14,22 @@ console.log(fruit.owner || 'Riza');
 
 // access with Object.keys
 
+// seach key
 const key = Object.values(fruit);
+// search values
 const values = Object.values(fruit);
 console.log('ini key :', key, 'ini value :', values);
+
+// seach key and value
+
+const entries = Object.entries(fruit);
+console.log(entries);
+
+// change value
+
+fruit.price = 12000;
+console.log(fruit);
+
+// delete key and value in Object.
+delete fruit.name;
+console.log(fruit);
